bba902ee886c9de241738ab1cb134f4abb53f8e2
keisuke <keisuke6402@yahoo.co.jp>
2020-05-29 14:47:54 +0900

    top

--- test/controllers/tops_controller_test.rb
+++ test/controllers/tops_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class TopsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get tops_index_url
+    assert_response :success
+  end
+
 end
